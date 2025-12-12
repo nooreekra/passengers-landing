f2c335af7021d8093817d45b69c44ea427540b39
nooreekra <rakhymzhan.ff@gmail.com>
2025-12-12 21:56:43 +0500

    feat: update mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-"use client"
-
 import LandingPage from './LandingPage'
 
+// Отключаем статическую генерацию для этой страницы
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default function Home() {
   return <LandingPage />
 }
-
